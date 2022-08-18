9c5940cdb1b87cdd313d400d4dd65a4ac226bd76
carissamero <carissamero@yahoo.com>
2022-08-17 19:08:34 -0500

    first save

---
+++ client/src/pages/ProfileDev.js
@@ -0,0 +1,390 @@
+import React from "react";
+import Axios from 'axios';
+import { useState } from "react";
+import { Form, Col, Row, Button, Modal } from "react-bootstrap";
+import { useParams, Redirect } from 'react-router-dom';
+import { useQuery, useMutation } from "@apollo/client";
+import { UPDATE_USER } from "../utils/mutations";
+import { QUERY_SINGLE_USER, QUERY_ME } from "../utils/queries";
+
+import Auth from "../utils/auth"
+
+export default function ProfileDev() {
+    const { userId: userParam } = useParams();
+
+    const { loading, data } = useQuery(userParam ? QUERY_SINGLE_USER : QUERY_ME, {
+        variables: { userId: userParam },
+    });
+
+    // console.log(data)
+    const user = data ? .me || data ? .user || {};
+
+    // console.log(data)
+    // console.log(user)
+    const [modalData, setModalData] = useState({})
+    const [updateUser, { error }] = useMutation(UPDATE_USER);
+    const [show, setShow] = useState(false);
+
+    if (Auth.loggedIn() && Auth.getProfile().data._id === userParam) {
+        return <Redirect to = "/me" / >
+    }
+
+    if (loading) {
+        return <div > Loading... < /div>
+    }
+
+    if (!user ? .fullName) {
+        return ( <
+            h4 style = {
+                { padding: '40px', textAlign: 'center', fontSize: '30px', margin: '65px', backgroundColor: '#4AB8B1', color: 'white' } } >
+            You need to be logged in to see your profile page.Use the navigation links above to sign up or log in !
+            <
+            /h4>
+        );
+    }
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        // console.log(value)
+        setModalData({...modalData, [name]: value, })
+    }
+
+    const handleIntegerChange = (event) => {
+        const { name, value } = event.target;
+        // console.log(value)
+        setModalData({...modalData, [name]: parseInt(value), })
+    }
+
+    const uploadImage = (files) => {
+        const formData = new FormData();
+        formData.append("file", files[0]);
+        formData.append("upload_preset", "vylq4hww");
+
+        Axios.post("https://api.cloudinary.com/v1_1/darylnauman/image/upload", formData)
+            .then((res) => {
+                const profileImageURL = res.data.secure_url;
+                setModalData({...modalData, profileImage: profileImageURL })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
+
+    const handleModalSubmit = async(event) => {
+        event.preventDefault();
+
+        // console.log(modalData)
+        try {
+            const { data } = await updateUser({
+                variables: { userData: {...modalData } }
+            })
+
+            handleClose()
+            window.location.reload()
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true);
+
+    return ( <
+        div className = "profiledevcont"
+        style = {
+            { backgroundColor: "#F0A202" } } >
+        <
+        h1 className = "mainh1"
+        style = {
+            { backgroundColor: '#F0A202', paddingTop: '50px' } } > PROFILE: < /h1> <
+        div style = {
+            { display: 'flex', justifyContent: 'center' } } >
+        <
+        img src = { user.profileImage }
+        style = {
+            { maxWidth: "500px", display: 'flex' } }
+        alt = { "user profile" } >
+        < /img> <
+        /div>
+
+        <
+        div className = 'profile' >
+        <
+        div className = "profile-info" >
+        <
+        h2 className = 'profiletitles' > Name: < /h2> <
+        p className = 'profiletext' > { user.fullName } < /p> <
+        h2 className = 'profiletitles' > Description: < /h2> <
+        p className = 'profiletext' > { user.userDescription } < /p> <
+        h2 className = 'profiletitles' > Type of developer: < /h2> <
+        p className = 'profiletext' > { user.primaryFocus } < /p> <
+        h2 className = 'profiletitles' > Skills: < /h2> <
+        p className = 'profiletext' > { user.skillSet } < /p> <
+        h2 className = 'profiletitles' > Hourly rate: < /h2> <
+        p className = 'profiletext' > $ { user.hourlyRate }
+        /hr</p >
+        <
+        h2 className = 'profiletitles' > Company: < /h2> <
+        p className = 'profiletext' > { user.company } < /p> <
+        h2 className = 'profiletitles' > Project name: < /h2> <
+        p className = 'profiletext' > { user.sampleProjectName } < /p> <
+        h2 className = 'profiletitles' > Project URL: < /h2> <
+        a className = 'profiletext'
+        href = { user.sampleProjectURL } > { user.sampleProjectURL } < /a> <
+        h2 className = 'profiletitles' > Linkedin URL: < /h2> <
+        a className = 'profiletext'
+        href = { user.linkedIn } > { user.linkedIn } < /a> <
+        h2 className = 'profiletitles' > Github URL < /h2> <
+        a className = 'profiletext'
+        href = { user.gitHub } > { user.gitHub } < /a> <
+        h2 className = 'profiletitles' > Services: < /h2> <
+        p className = 'profiletext' > { user.servicesOffered } < /p> <
+        h2 className = 'profiletitles' > Contact Info: < /h2> <
+        p className = 'profiletext' > { user.email } < /p> <
+        Button variant = "primary"
+        onClick = { handleShow }
+        style = {
+            {
+                backgroundColor: "#4AB8B1",
+                border: "none",
+                fontSize: "20px",
+                padding: "12px 24px",
+                marginTop: "15px",
+            }
+        } >
+        Edit Profile <
+        /Button> <
+        /div> <
+        /div>
+
+        <
+        Modal show = { show }
+        onHide = { handleClose } >
+        <
+        Modal.Header closeButton >
+        <
+        Modal.Title > Edit Profile < /Modal.Title> <
+        /Modal.Header> <
+        Form className = "profileform"
+        action = "/upload"
+        method = "POST"
+        enctype = "multipart/form-data" >
+        <
+        Form.Group controlId = "formFile"
+        className = "mb-3" >
+        <
+        Form.Label > Upload profile image file < /Form.Label> <
+        Form.Control type = "file"
+        accept = "image/*"
+        onChange = {
+            (event) => {
+                uploadImage(event.target.files);
+            }
+        }
+        /> <
+        /Form.Group>
+
+        <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextEmail" >
+        <
+        Form.Label className = "formlabel" > Full Name < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "text"
+        placeholder = { user.fullName }
+        value = { modalData.fullName }
+        name = "fullName"
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        style = {
+            { paddingBottom: '10px' } }
+        className = "mb-3"
+        controlId = "formPlaintextEmail" >
+        <
+        Form.Label > What type of web developer are you ? < /Form.Label> <
+        Form.Select aria - label = "Floating label select example"
+        name = "primaryFocus"
+        onChange = { handleInputChange } >
+        <
+        option style = {
+            { color: 'lightgray' } }
+        value = { modalData.primaryFocus } > Open this menu to select < /option> <
+        option value = "Front-End" > Front - End < /option> <
+        option value = "Back-End" > Back - End < /option> <
+        option value = "Full-Stack" > Full - Stack < /option> <
+        /Form.Select> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Profile description < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "text"
+        placeholder = { user.userDescription }
+        name = "userDescription"
+        value = { modalData.userDescription }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Skills < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        placeholder = { user.skillSet }
+        name = "skillSet"
+        value = { modalData.skillSet }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Hourly rate < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "text"
+        placeholder = { user.hourlyRate }
+        name = "hourlyRate"
+        value = { modalData.hourlyRate }
+        onChange = { handleIntegerChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Company < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        placeholder = { user.company }
+        name = "company"
+        value = { modalData.company }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Project example URL < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "sampleProjectURL"
+        placeholder = { user.sampleProjectURL }
+        value = { modalData.sampleProjectURL }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Project example name < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "sampleProjectName"
+        placeholder = { user.sampleProjectName }
+        value = { modalData.sampleProjectName }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Your Linkedin URL < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "linkedIn"
+        placeholder = { user.linkedIn }
+        value = { modalData.linkedIn }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Your Resume URL < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "resumeURL"
+        placeholder = { user.resumeURL }
+        value = { modalData.resumeURL }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Your GitHub URL < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "gitHub"
+        placeholder = { user.gitHub }
+        value = { modalData.gitHub }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Form.Group as = { Row }
+        className = "mb-3"
+        controlId = "formPlaintextPassword" >
+        <
+        Form.Label > Services < /Form.Label> <
+        Col sm = "30" >
+        <
+        Form.Control type = "skills"
+        name = "servicesOffered"
+        placeholder = { user.servicesOffered }
+        value = { modalData.servicesOffered }
+        onChange = { handleInputChange }
+        /> <
+        /Col> <
+        /Form.Group> <
+        Button variant = "primary"
+        onClick = { handleModalSubmit }
+        style = {
+            {
+                margin: "0px 45px 45px 45px",
+                backgroundColor: "#4AB8B1",
+                border: "none",
+            }
+        } >
+        Save Changes <
+        /Button> <
+        /Form>
+
+        <
+        /Modal> <
+        /div>
+    );
+}
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
+export default function Signup() {
+    const [formState, setFormState] = useState({
+        fullName: '',
+        email: '',
+        role: '',
+        password: '',
+        company: ''
+    });
+    const [addUser, { error, data }] = useMutation(ADD_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async(event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addUser({
+                variables: {...formState },
+            });
+
+            Auth.login(data.addUser.token);
+
+            if (data.addUser.user.role === "developer") {
+                window.location.assign('/homedev');
+            } else {
+                window.location.assign('/homecli');
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    return ( <
+        div style = {
+            { backgroundColor: '#f2f7f2ff', padding: '10px' } } >
+        <
+        div className = 'login-container'
+        style = {
+            { backgroundColor: '#294A66', border: 'none', marginTop: '100px', padding: '30px 20px 50px' } } >
+        <
+        p style = {
+            { color: '#F0A202', textAlign: 'center', fontSize: '30px' } } >
+        Create your < span style = {
+            { fontWeight: 'bold' } } > Find My Coder < /span> account! <
+        /p> <
+        Form className = 'login-form'
+        style = {
+            { backgroundColor: '#F0A202' } } >
+        <
+        Form.Group >
+
+        <
+        Form.Label style = {
+            { margin: '5px 0 0', fontSize: '20px', Height: '10px' } } > Full Name < /Form.Label> <
+        Form.Control type = 'text'
+        placeholder = 'Full name'
+        onChange = { handleChange }
+        name = "fullName"
+        value = { formState.fullName } > < /Form.Control>
+
+        <
+        Form.Label style = {
+            { margin: '5px 0', fontSize: '20px' } } > Email < /Form.Label> <
+        Form.Control type = 'email'
+        placeholder = 'email'
+        onChange = { handleChange }
+        name = "email"
+        value = { formState.email } > < /Form.Control>
+
+        <
+        Form.Label style = {
+            { margin: '5px 0', fontSize: '20px' } } > Password < /Form.Label> <
+        Form.Control name = 'password'
+        type = 'password'
+        placeholder = 'password'
+        value = { formState.password }
+        onChange = { handleChange } > < /Form.Control>
+
+        <
+        Form.Label style = {
+            { margin: '5px 0', fontSize: '20px' } } > Developer / Client < /Form.Label> <
+        Form.Select name = "role"
+        onChange = { handleChange } >
+        <
+        option > Select user type < /option> <
+        option value = "developer" > Developer < /option> <
+        option value = "client" > Client < /option> <
+        /Form.Select>
+
+        <
+        Form.Label style = {
+            { margin: '5px 0', fontSize: '20px' } } > Company < span style = {
+            { fontStyle: 'italic' } } > (optional) < /span></Form.Label >
+        <
+        Form.Control type = 'text'
+        placeholder = 'company name'
+        name = "company"
+        value = { formState.company }
+        onChange = { handleChange } > < /Form.Control>                    
+
+        <
+        /Form.Group> <
+        Button style = {
+            { backgroundColor: '#294A66', border: 'none', fontSize: '20px', padding: '12px 24px', marginTop: '15px' } }
+        onClick = { handleFormSubmit } > Sign Up < /Button>    <
+        /Form> <
+        /div> <
+        /div>
+    );
+}
