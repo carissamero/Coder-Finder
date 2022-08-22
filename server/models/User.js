4b15b037cf98ccd24effcc019f3efc18b37d3d83
carissamero <carissamero@yahoo.com>
2022-08-22 15:41:51 -0500

    fixed user seeds

---
+++ server/models/User.js
@@ -0,0 +1,80 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const url = 'https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg'
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    role: {
+        type: String,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        trim: true,
+    },
+    company: {
+        type: String,
+        trim: true,
+    },
+    profileImage: {
+        type: String,
+        default: url
+    },
+    userDescription: {
+        type: String,
+    },
+    sampleProjectName: {
+        type: String
+    },
+    sampleProjectURL: {
+        type: String
+    },
+    resumeURL: {
+        type: String
+    },
+    primaryFocus: {
+        type: String
+    },
+    skillSet: {
+        type: String
+    },
+    hourlyRate: {
+        type: Number
+    },
+    linkedIn: {
+        type: String
+    },
+    gitHub: {
+        type: String
+    },
+    servicesOffered: {
+        type: String
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,56 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require("../models/");
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+    Query: {
+        users: async() => {
+            return await User.find();
+        },
+        user: async(parent, { userId }) => {
+            return await User.findOne({ _id: userId });
+        },
+        me: async(parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+
+    Mutation: {
+        addUser: async(
+            parent, { email, password, role, fullName, company }
+        ) => {
+            const user = await User.create({ email, password, role, fullName, company });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user with this email found!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        updateUser: async(
+            parent, args, context
+        ) => {
+            return await User.findByIdAndUpdate(context.user._id, { $set: args.userData }, { new: true })
+        }
+    },
+};
+
+module.exports = resolvers;
