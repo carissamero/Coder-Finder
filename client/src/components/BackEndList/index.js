3b5ddaaac7a1ba33215d481f3f4efc473006fac6
kekehua <100812740+kekehua@users.noreply.github.com>
2022-08-22 14:19:54 -0500

    formatting finished

--- client/src/components/BackEndList/index.js
+++ client/src/components/BackEndList/index.js
@@ -3,50 +3,45 @@ import { Card, Row } from "react-bootstrap"
 
 const BEDeveloperList = ({ bedevelopers }) => {
 
-    return ( <
-        div >
-        <
-        Row className = "developersmain"
-        style = {
-            { padding: '10px 30px 30px 30px', display: 'flex', justifyContent: 'center' } } >
+    return (<div >
+        <Row className="developersmain"
+            style={
+                { padding: '10px 30px 30px 30px', display: 'flex', justifyContent: 'center' }} >
 
-        {
-            bedevelopers.slice(0, 5).map((bedeveloper) => ( <
-                Card className = "m-4"
-                style = {
-                    { width: "18em" } }
-                key = { bedeveloper.id } >
-                <
-                Card.Body style = {
-                    { textAlign: 'center', padding: '30px 30px 40px 30px' } } >
-                <
-                Card.Title style = {
-                    { color: 'black', padding: '10px' } } > { bedeveloper.fullName } < /Card.Title> <
-                Card.Img variant = "top"
-                src = { bedeveloper.profileImage }
-                style = {
-                    { maxWidth: '284px' } }
-                /> <
-                Card.Text style = {
-                    { color: 'black', paddingTop: '16px' } } > { bedeveloper.userDescription } < /Card.Text> <
-                Card.Subtitle style = {
-                    { padding: '20px 20px 30px 20px' } }
-                className = "mb-2 text-muted" > Hourly rate: $ { bedeveloper.hourlyRate }
-                /hr</Card.Subtitle >
-                <
-                a className = "infobut"
-                variant = "primary"
-                style = {
-                    { margin: '10px' } }
-                href = { `/profileclient/${bedeveloper._id}` } >
-                View profile <
-                /a> <
-                /Card.Body> <
-                /Card>
-            ))
-        } <
-        /Row> <
-        /div>
+            {
+                bedevelopers.slice(0, 5).map((bedeveloper) => (
+                    <Card className="m-4"
+                        style={
+                            { width: "18em" }}
+                        key={bedeveloper.id} >
+                        <Card.Body style={
+                            { textAlign: 'center', padding: '30px 30px 40px 30px' }} >
+                            <Card.Title style={
+                                { color: 'black', padding: '10px' }} > {bedeveloper.fullName}
+                            </Card.Title>
+                            <Card.Img variant="top"
+                                src={bedeveloper.profileImage}
+                                style={
+                                    { maxWidth: '284px' }}/>
+                            <Card.Text style={
+                                { color: 'black', paddingTop: '16px' }} > {bedeveloper.userDescription} 
+                            </Card.Text>
+                            <Card.Subtitle style={
+                                { padding: '20px 20px 30px 20px' }}
+                                className="mb-2 text-muted" > Hourly rate: $ {bedeveloper.hourlyRate}/hr
+                            </Card.Subtitle >
+                            <a className="infobut"
+                                variant="primary"
+                                style={
+                                    { margin: '10px' }}
+                                href={`/profileclient/${bedeveloper._id}`} >
+                                View profile
+                            </a>
+                        </Card.Body>
+                    </Card>
+                ))
+            } </Row>
+    </div>
     );
 };
 
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,66 +4,52 @@ import { AiFillGithub, AiFillTwitterCircle, AiFillInstagram } from "react-icons/
 import { Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
-    return ( <
-        div className = 'main-footer' >
-        <
-        div className = 'container' >
-        <
-        Row style = {
-            { textAlign: 'center' } } >
-        <
-        Col >
-        <
-        h3 style = {
-            { fontWeight: '200' } } > Coder Finder < /h3> <
-        ul className = 'footer-items' >
-        <
-        li >
-        <
-        a style = {
-            { color: '#ffff', fontSize: '20px', textDecoration: 'none' } }
-        href = 'https://github.com/carissamero/Coder-Finder.git' >
-        GitHub < AiFillGithub style = {
-            { marginRight: '15px' } }
-        /> <
-        /a> <
-        /li> <
-        li >
-        <
-        a style = {
-            { color: '#ffff', fontSize: '20px', textDecoration: 'none' } }
-        href = 'https://twitter.com/404' >
-        Twitter < AiFillTwitterCircle style = {
-            { marginRight: '15px' } }
-        /> <
-        /a> <
-        /li> <
-        li >
-        <
-        a style = {
-            { color: '#ffff', fontSize: '20px', textDecoration: 'none' } }
-        href = 'https://instagram.com/404' >
-        Instagram < AiFillInstagram style = {
-            { marginRight: '15px' } }
-        /> <
-        /a>                                 <
-        /li> <
-        /ul> <
-        /Col>                     <
-        /Row> <
-        hr / >
-        <
-        Row style = {
-            { textAlign: 'center' } } >
-        <
-        p >
-        &
-        copy; { new Date().getFullYear() }
-        Coder Finder | C.M.A.G.Development Inc | All rights reserved <
-        /p>            <
-        /Row> <
-        /div> <
-        /div>
+    return (<div className='main-footer' >
+        <div className='container' >
+            <Row style={
+                { textAlign: 'center' }} >
+                <Col >
+                    <h3 style={
+                        { fontWeight: '200' }} > Coder Finder </h3>
+                    <ul className='footer-items' >
+                        <li >
+                            <a style={
+                                { color: '#ffff', fontSize: '20px', textDecoration: 'none' }}
+                                href='https://github.com/carissamero/Coder-Finder.git' >
+                                GitHub < AiFillGithub style={
+                                    { marginRight: '15px' }}
+                                /> </a>
+                        </li>
+                        <li >
+                            <a style={
+                                { color: '#ffff', fontSize: '20px', textDecoration: 'none' }}
+                                href='https://twitter.com/404' >
+                                Twitter < AiFillTwitterCircle style={
+                                    { marginRight: '15px' }}
+                                />
+                            </a>
+                        </li>
+                        <li >
+                            <a style={
+                                { color: '#ffff', fontSize: '20px', textDecoration: 'none' }}
+                                href='https://instagram.com/404' >
+                                Instagram < AiFillInstagram style={
+                                    { marginRight: '15px' }}
+                                />
+                            </a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+            <hr/>
+            <Row style={
+                { textAlign: 'center' }} >
+                <p >&copy; {new Date().getFullYear()}
+                    Coder Finder | C.M.A.G.Development Inc | All rights reserved
+                </p>
+            </Row>
+        </div>
+    </div>
     )
 }
 export default Footer;
